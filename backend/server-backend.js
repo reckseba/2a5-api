7ec30433dff3ad16e528067911397926ec1a63ff
Sebastian Reck <mail@s-reck.de>
2021-11-16 21:41:00 +0100

    remove https as it gets served by nginx

--- backend/server-backend.js
+++ backend/server-backend.js
@@ -1,23 +1,18 @@
-const fs = require('fs');
 const http = require('http');
-const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./routes/api');
-const path = require('path');
 require('dotenv').config({path: './config/process.env'});
-
-let credentials = {};
 
 // new express server
 const app = express();
 
 // prepare database link
-const dblink = "mongodb+srv://" + process.env.DBUSERNAME + ":" + process.env.DBPASSWORD + "@" + process.env.DBCLUSTER + "." + process.env.DBSERVER + ".mongodb.net/" + process.env.DBDATABASENAME + "?retryWrites=true&w=majority"
+const db = "mongodb+srv://" + process.env.DBUSERNAME + ":" + process.env.DBPASSWORD + "@" + process.env.DBCLUSTER + "." + process.env.DBSERVER + ".mongodb.net/" + process.env.DBDATABASENAME + "?retryWrites=true&w=majority"
 
 // connect to the database
-mongoose.connect(dblink, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log('Database connected successfully'))
   .catch(err => console.log(err));
 
@@ -39,37 +34,9 @@ app.use((err, req, res, next) => {
   next();
 });
 
-if(process.env.PROTOCOL == "https") {
 
-  // Certificate
-  if(["test", "local"].includes(process.env.ENVIRONMENT)) {
-    credentials.key = fs.readFileSync(`./ssl/test.privkey.pem`, 'utf8');
-    credentials.cert = fs.readFileSync(`./ssl/test.cert.pem`, 'utf8');
-  }
+const httpServer = http.createServer(app);
 
-  // if it's production then we want the ca chain cert to be included
-	if(process.env.ENVIRONMENT == "prod") {
-    credentials.key = fs.readFileSync(`./ssl/privkey.pem`, 'utf8');
-    credentials.cert = fs.readFileSync(`./ssl/cert.pem`, 'utf8');
-		credentials.ca = fs.readFileSync(`./ssl/chain.pem`, 'utf8');
-	}
-
-  // start the https server
-  const httpsServer = https.createServer(credentials, app);
-
-  // bind port we defined in config file
-  httpsServer.listen(process.env.PORTBACKEND, () => {
-    console.log(`HTTPS Prod-Server running on port ${process.env.PORTBACKEND}`);
-  });
-
-} else if(process.env.PROTOCOL == "http") {
-
-  const httpServer = http.createServer(app);
-
-  httpServer.listen(process.env.PORTBACKEND, () => {
-    console.log(`HTTP Test-Server running on port ${process.env.PORTBACKEND}`);
-  });
-
-} else {
-  console.log("Please define a protocol in your config file.");
-}
+httpServer.listen(process.env.PORTBACKEND, () => {
+  console.log(`HTTP ${process.env.ENVIRONMENT} Backend-Server running on port ${process.env.PORTBACKEND}`);
+});
--- frontend/server/server-frontend.js
+++ frontend/server/server-frontend.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const https = require('https');
-const fs = require('fs');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const helmet = require('helmet');
-const crypto = require('crypto');
 require('dotenv').config({path: './config/process.env'});
 
 // init the express server
@@ -13,105 +9,44 @@ const app = express();
 
 // prepare a filter for incoming short URL requests. Those we need to catch and proxy to backend
 const filter = function (pathname, req) {
-        const regex = /^\/[A-Za-z0-9]{3,5}/gm;
-        return pathname.match(regex) && pathname.length >= (3+1) && pathname.length <= (5+1) && req.method === 'GET';
+    const regex = /^\/[A-Za-z0-9]{3,5}/gm;
+    return pathname.match(regex) && pathname.length >= (3+1) && pathname.length <= (5+1) && req.method === 'GET';
 };
 
-let credentials = {};
-
 // http-proxy-middleware can detect self signed certificates. If we detect such, we block to preved mitm attacks
-let secureProxy = true;
-
-if(process.env.PROTOCOL == "https") {
-
-	if(["test", "local"].includes(process.env.ENVIRONMENT)) {
-		// Certificate
-		credentials.key = fs.readFileSync(`./ssl/test.privkey.pem`, 'utf8');
-		credentials.cert = fs.readFileSync(`./ssl/test.cert.pem`, 'utf8');
-		
-		// but if we are testing: We only have self signed certificates
-		secureProxy = false;
-	}
-
-	if(process.env.ENVIRONMENT == "prod") {
-
-		// request was via http, so redirect to https
-		app.use (function (req, res, next) {
-			if (req.secure) {
-				next();
-			} else {
-				res.redirect('https://' + req.headers.host + req.url);
-			}
-		});
-
-		credentials.key = fs.readFileSync(`./ssl/privkey.pem`, 'utf8');
-		credentials.cert = fs.readFileSync(`./ssl/cert.pem`, 'utf8');
-		credentials.ca = fs.readFileSync(`./ssl/chain.pem`, 'utf8');
-		
-		app.use((req, res, next) => {
-			res.locals.cspNonce = crypto.randomBytes(16).toString("hex");
-			next();
-		});
-
-		app.use(
-			helmet.contentSecurityPolicy({
-				useDefaults: true,
-				directives: {
-					scriptSrc: [
-						"'self'", 
-						(req, res) => `'nonce-${res.locals.cspNonce}'`
-					],
-				},
-			})
-		);
-
-	}
-
-}
+let secureProxy = false;
 
 app.use(
 	'/newUrlLong',
 	createProxyMiddleware({
-		target: `${process.env.PROTOCOL}://${process.env.BACKENDHOST}:${process.env.PORTBACKEND}`,
+		target: `http://${process.env.BACKENDHOST}:${process.env.PORTBACKEND}`,
 		changeOrigin: true,
 		secure: secureProxy
 	})
 );
 
+// route short urls to our backend to read from db what's behind it
 app.use(
 	'/*',
 	createProxyMiddleware(filter, {
-		target: `${process.env.PROTOCOL}://${process.env.BACKENDHOST}:${process.env.PORTBACKEND}`,
+		target: `http://${process.env.BACKENDHOST}:${process.env.PORTBACKEND}`,
 		changeOrigin: true,
 		secure: secureProxy
 	})
 );
 
-
+// load static webpage from folder 'build'
 app.use(express.static(path.join(__dirname, 'build')));
 
+// index route 2a5.de/ sends index.html from build folder
 app.get('/', function (req, res) {
 	  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// start https server if defined in config file
-
-if(process.env.PROTOCOL == "https") {
-
-	const httpsServer = https.createServer(credentials, app);
-
-	httpsServer.listen(process.env.PORTFRONTENDHTTPS, () => {
-		console.log(`HTTPS Prod-Server running on port ${process.env.PORTFRONTENDHTTPS}`);
-	});
-
-}
-
-// we launch an http server in all cases. For testing this will be the one we use to
-// interact with our application. But in https mode this will be the one which redirects traffic to https
-
+// start express http server - ssl will get handled by nginx or apache
 const httpServer = http.createServer(app);
 
+// listen on specified front-end-port
 httpServer.listen(process.env.PORTFRONTENDHTTP, () => {
-	console.log(`HTTP Prod-Server running on port ${process.env.PORTFRONTENDHTTP}`);
+	console.log(`HTTP ${process.env.ENVIRONMENT} Frontend-Server running on port ${process.env.PORTFRONTENDHTTP}`);
 });
-
